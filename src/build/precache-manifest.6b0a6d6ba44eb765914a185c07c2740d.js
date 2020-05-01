95578e3481281bf572f6405464a52bdac71885ca
Jessie Marinau <60623677+OneWugTwoWugs@users.noreply.github.com>
2020-05-01 17:40:10 -0500

    First commit

---
+++ src/build/precache-manifest.6b0a6d6ba44eb765914a185c07c2740d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b2588559038f03d89b1a09b9965a0b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd48ede472123e523aa8",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "0cf6d6148858952f572d",
+    "url": "/static/js/2.f2b8de2a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f2b8de2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd48ede472123e523aa8",
+    "url": "/static/js/main.7da0dd4d.chunk.js"
+  },
+  {
+    "revision": "c6cd80a6fce542ddaee9",
+    "url": "/static/js/runtime-main.b9244b71.js"
+  }
+]);
